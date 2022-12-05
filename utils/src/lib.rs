199d0707b424e28e38b295c46002c4fd2677dd34
MrLento234 <lento.manickathan@gmail.com>
2022-12-05 21:03:51 +0100

    added utils

---
+++ utils/src/lib.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+pub fn parse_file(path: &str) -> String {
+    fs::read_to_string(path).expect("Cannot find file!")
+}
+
+pub fn color_text(text: &str, color: char) -> String {
+    let ansi_start: &str = match color {
+        'g' => "\u{1b}[32m",
+        'b' => "\u{1b}[34m",
+        _ => ""
+    };
+    let ansi_end: &str = "\u{1b}[39m";
+    format!("{}{}{}", ansi_start, text, ansi_end)
+}
