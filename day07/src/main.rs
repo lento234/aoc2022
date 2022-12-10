562684acab69c4611a1b315af59c386dc41b88cf
MrLento234 <lento.manickathan@gmail.com>
2022-12-11 00:12:48 +0100

    solved part 2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -3,10 +3,7 @@ use std::collections::HashMap;
 use std::rc::Rc;
 use std::time::Instant;
 
-fn part_1(path: &str) -> usize {
-    // Collect file contents
-    let content = utils::parse_file(path);
-
+fn generate_fs(content: String) -> Rc<Dir> {
     // Create root node
     let root = Rc::new(Dir::new());
     // Create a pointer the current working directory
@@ -30,6 +27,15 @@ fn part_1(path: &str) -> usize {
         }
     }
 
+    root
+}
+
+fn part_1(path: &str) -> usize {
+    // Collect file contents
+    let content = utils::parse_file(path);
+
+    let root = generate_fs(content);
+
     // Travel to all nodes in the directory starting from the root
     let mut total_size: usize = 0;
     let mut queue = Vec::new();
@@ -41,11 +47,46 @@ fn part_1(path: &str) -> usize {
         }
 
         let size = node.get_size();
-        if size < 100000 {
+        if size < 100_000 {
             total_size += size;
         }
     }
     total_size
+}
+
+fn part_2(path: &str) -> Option<usize> {
+    // Collect file contents
+    let content = utils::parse_file(path);
+
+    let root = generate_fs(content);
+
+    // Travel to all nodes in the directory starting from the root
+    let total_space = 70_000_000_usize;
+    let space_needed = 30_000_000_usize;
+
+    let available_space = root.get_size();
+    let unused_space = total_space - available_space;
+
+    let minimum_space_needed = space_needed - unused_space;
+
+    let mut queue = Vec::new();
+    queue.push(Rc::clone(&root));
+
+    let mut sizes = Vec::new();
+    while let Some(node) = queue.pop() {
+        for subdir in node.subdir.borrow().values() {
+            queue.push(Rc::clone(subdir));
+        }
+
+        sizes.push(node.get_size());
+    }
+    sizes.sort();
+    for size in sizes {
+        if size > minimum_space_needed {
+            return Some(size);
+        }
+    }
+    None
 }
 
 fn main() {
@@ -61,8 +102,12 @@ fn main() {
         utils::color_text("[Part 1]", 'g'),
         part_1("input.txt")
     );
-    // // Challenge 2
-    // println!("{}: {}", utils::color_text("[Part 2]", 'g'), part_2("input.txt"));
+    // Challenge 2
+    println!(
+        "{}: {}",
+        utils::color_text("[Part 2]", 'g'),
+        part_2("input.txt").unwrap()
+    );
 
     println!(
         "{}: {}",
@@ -78,6 +123,11 @@ mod test {
     #[test]
     fn test_part1() {
         assert!(part_1("test_input.txt") == 95437);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert!(part_2("test_input.txt") == Some(24933642));
     }
 }
 
